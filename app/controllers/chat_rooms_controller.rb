a1e3534f3945dd04cf9ba16c92df802e1ae6acdb
David Kennell <davidkennell0@gmail.com>
2017-11-11 20:42:41 -0500

    misc debugging

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -4,6 +4,9 @@ class ChatRoomsController < ApplicationController
     @chat_rooms = ChatRoomDecorator.decorate_collection(ChatRoom.all)
   end
 
-  
+  def show
+  	@chat_room = ChatRoom.find(params[:id])
+  	@json_object = ChatRoomsSerializer.new(@chat_room).as_json
+  end
 
 end
